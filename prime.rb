5613038902d16db20dbeaa0bb56e33f90df5f854
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-05 04:09:06 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,9 +1,14 @@
 # Add  code here!
 def prime?(num)
-  if num < 2
-    false
-  else
+  if num > 2
     array_Rango = (2..num/2).to_a
     array_Rango.all?{ |n| num % n != 0}
   end
 end
+
+#if num < 2
+#  false
+#else
+#  array_Rango = (2..num/2).to_a
+#  array_Rango.all?{ |n| num % n != 0}
+#end
